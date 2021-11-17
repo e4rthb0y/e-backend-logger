3d108a6b2a7c4bc3142f6e9f4f58c25d96afd4fe
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2021-11-17 07:09:33 -0600

    build: create RESTful API for Log model

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,26 +1,109 @@
 'use strinct';
 
+const Joi = require('joi');
+const Log = require('../models/Log');
+
 class MainController {
 
-	all(req, res, next) {
-		res.json({ message: 'Example request.' });
-	}
+  _authenticate(req, res, next) {
+    const njwt = require('njwt');
 
-	create(req, res, next) {
-		res.json({ message: 'Example request.' });
-	}
+    const signingkey = process.env.APP_KEY.toString('base64');
 
-	info(req, res, next) {
-		res.json({ message: 'Example request.' });
-	}
+    // Assume Bearer authentication
+    const token = req.headers.authorization.replace(/^Bearer /g, '');
 
-	update(req, res, next) {
-		res.json({ message: 'Example request.' });
-	}
+    njwt.verify(token, signingkey, (err, verifiedToken) => {
+      if (err) {
+        res.json({ message: err.message });
+      } else {
+        next();
+      }
+    });
+  }
 
-	delete(req, res, next) {
-		res.json({ message: 'Example request.' });
-	}
+  static _validateRequest = (req, next, schema) => {
+    const { error, value } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      next(
+        `Validation error: ${error.details.map((err) => err.message).join(', ')}`
+      );
+    } else {
+      req.body = value;
+      next();
+    }
+  }
+
+  all(req, res, next) {
+    Log.find()
+      .then((response) => res.json(response))
+      .catch(next);
+  }
+
+  _createLogSchema(req, res, next) {
+    const schema = Joi.object({
+      type: Joi.string()
+        .pattern(/(?:error|info|warning)/)
+        .required(),
+      priority: Joi.string()
+        .pattern(/(?:lowest|low|medium|high|highest)/)
+        .required(),
+      path: Joi.string().required(),
+      message: Joi.string().required(),
+      request: Joi.object({
+        data: Joi.any().required(),
+      }).required(),
+      response: Joi.object({
+        data: Joi.any().required(),
+      }).required(),
+    });
+    MainController._validateRequest(req, next, schema);
+  }
+
+  create(req, res, next) {
+    Log.create(req.body)
+      .then((response) => res.json(response))
+      .catch(next);
+  }
+
+  info(req, res, next) {
+    Log.findOne({ _id: req.params.id })
+      .then((response) => res.json(response))
+      .catch(next);
+  }
+
+  _updateLogSchema(req, res, next) {
+    const schema = Joi.object({
+      type: Joi.string()
+        .pattern(/(?:error|info|warning)/)
+        .empty(''),
+      priority: Joi.string()
+        .pattern(/(?:lowest|low|medium|high|highest)/)
+        .empty(''),
+      path: Joi.string().empty(''),
+      message: Joi.string().empty(''),
+      request: Joi.object({
+        data: Joi.any(),
+      }).empty(''),
+      response: Joi.object({
+        data: Joi.any(),
+      }).empty(''),
+    });
+    MainController._validateRequest(req, next, schema);
+  }
+
+  update(req, res, next) {
+    Log.updateOne({ _id: req.params.id }, req.body)
+      .then((response) => res.json(response))
+      .catch(next);
+  }
+
+  delete(req, res, next) {
+    Log.deleteOne({ _id: req.params.id })
+      .then((response) => res.json(response))
+      .catch(next);
+  }
 }
 
 module.exports = new MainController();
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const router = require('express').Router();
-const prefix = '';
+const prefix = 'logs';
 
 const controller = require('../controllers/main.controller');
 
-router.get(`${prefix}/`, controller.all);
-router.post(`${prefix}/`, controller.create);
-router.get(`${prefix}/:id`, controller.info);
-router.put(`${prefix}/:id`, controller.update);
-router.delete(`${prefix}/:id`, controller.delete);
+// Authenticate all routes
+router.use(controller._authenticate);
+
+router.get(`/${prefix}/`, controller.all);
+router.post(`/${prefix}/`, controller._createLogSchema, controller.create);
+router.get(`/${prefix}/:id`, controller.info);
+router.put(`/${prefix}/:id`, controller._updateLogSchema, controller.update);
+router.delete(`/${prefix}/:id`, controller.delete);
 
 module.exports = router;
